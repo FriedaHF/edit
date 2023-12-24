bbc87940101666b6046aa799740995c0a7da0f9a
Frieda Fry <friedah.fry@gmail.com>
2023-12-24 15:08:03 -0500

    i had to copy from another repo due to github messing up

---
+++ Develop/client/src/js/install.js
@@ -0,0 +1,30 @@
+const butInstall = document.getElementById("buttonInstall");
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('hit')
+    console.log("event" + event)
+    event.preventDefault();
+    window.deferredPrompt = event;
+
+    butInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+
+    // Show prompt
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+    // Clear prompt
+    console.log('install hit')
+    window.deferredPrompt = null;
+}); 
